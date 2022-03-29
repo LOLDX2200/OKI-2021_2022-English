bbd5f6fdf416d818988501c1d5420371ff954f56
LOLDX2200 <102176810+LOLDX2200@users.noreply.github.com>
2022-03-29 19:02:44 +0200

    Add files via upload

---
+++ mqueue.cpp
@@ -0,0 +1,24 @@
+//class implementing queue in C++
+//the same functionality as STL (Standard Template Library ) class queue in C++ 
+//with usage example
+//Version 1.0
+Consists
+a. Program template
+b. defintion of class mqueue 
+Author: Daniel Olkowski / Olimpijskie Kolo Informatyczne
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class mqueue {
+ vector<int> elements;
+ int first, last;
+};	
+
+int main() {
+ mqueue	payments;
+
+ return 0;
+}
